1e66e2ab4d7d0de06777250c8e5032decf116069
Sil-ba <barnova@spseke.sk>
2023-06-20 12:47:32 +0200

    funkcia na pokazenie reaktora

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -10,29 +10,38 @@ public class Reactor extends AbstractActor {
     private int damage;
     private Animation normalAnimation;
     public Reactor(){
-        temperature = 0;
-        state = false;
-        damage = 0;
-        normalAnimation = new Animation(
+        this.temperature = 0;
+        this.state = false;
+        this.damage = 0;
+        this.normalAnimation = new Animation(
             "sprites/reactor_on.png",
             80,80,
             0.1F,
             Animation.PlayMode.LOOP_PINGPONG
         );
 
-
-        setAnimation(normalAnimation);
+        setAnimation(this.normalAnimation);
     }
 
     public int getTemperature(){
-        return temperature;
+        return this.temperature;
     }
 
     public int getDamage(){
-        return damage;
+        return this.damage;
     }
 
     public void increaseTemperature(int increment){
+        this.temperature += increment;
+
+       // when temperature is above 2000, then damage increase
+        if (this.temperature >= 2000 && this.temperature <= 6000 ) {
+            int damage = this.temperature / 40 -50;
+
+            if(this.damage < damage ){
+                this.damage = damage;
+            }
+        }
 
     }
 }
