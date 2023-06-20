c2c86309368ab8533b1cfe9c78f8abf1634ebfca
Sil-ba <barnova@spseke.sk>
2023-06-20 11:32:45 +0200

    reaktor

---
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -0,0 +1,26 @@
+package sk.tuke.kpi.oop.game;
+//nazov balicka
+
+import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.graphics.Animation;
+
+public class Reactor extends AbstractActor {
+    private int temperature;
+    private boolean state;
+    private int damage;
+    private Animation normalAnimation;
+    public Reactor(){
+        temperature = 0;
+        state = false;
+        damage = 0;
+        normalAnimation = new Animation(
+            "sprites/reactor_on.png",
+            80,80,
+            0.1F,
+            Animation.PlayMode.LOOP_PINGPONG
+        );
+
+
+        setAnimation(normalAnimation);
+    }
+}
