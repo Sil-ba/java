55f41cb6c3a9c696f9efa1e7385ff1d5257c2b29
Sil-ba <barnova@spseke.sk>
2023-06-21 09:46:16 +0200

    vytvorenie clenskych premennych pre animacie

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -9,13 +9,31 @@ public class Reactor extends AbstractActor {
     private boolean state;
     private int damage;
     private Animation normalAnimation;
-    public Reactor(){
+    private Animation hotAnimation;
+    private Animation brokenAnimation;
+
+
+    public Reactor() {
         this.temperature = 0;
         this.state = false;
         this.damage = 0;
         this.normalAnimation = new Animation(
             "sprites/reactor_on.png",
-            80,80,
+            80, 80,
+            0.1F,
+            Animation.PlayMode.LOOP_PINGPONG
+        );
+
+        this.hotAnimation = new Animation(
+            "sprites/reactor_hot.png",
+            80, 80,
+            0.1F,
+            Animation.PlayMode.LOOP_PINGPONG
+        );
+
+        this.brokenAnimation = new Animation(
+            "sprites/reactor_broken.png",
+            80, 80,
             0.1F,
             Animation.PlayMode.LOOP_PINGPONG
         );
@@ -23,25 +41,29 @@ public class Reactor extends AbstractActor {
         setAnimation(this.normalAnimation);
     }
 
-    public int getTemperature(){
+    public int getTemperature() {
         return this.temperature;
     }
 
-    public int getDamage(){
+    public int getDamage() {
         return this.damage;
     }
 
-    public void increaseTemperature(int increment){
-        this.temperature += increment;
+    public void increaseTemperature(int increment) {
+        this.temperature = this.temperature + increment;
 
-       // when temperature is above 2000, then damage increase
-        if (this.temperature >= 2000 && this.temperature <= 6000 ) {
-            int damage = this.temperature / 40 -50;
+        // update animation
+        // if temperature is >= 6000, then broken show reactor
+        if (this.temperature >= 6000) {
+            setAnimation(this.brokenAnimation);
 
-            if(this.damage < damage ){
-                this.damage = damage;
-            }
+       // if (4000 <= temperature < 6000), then show hot reactor
+        } else if (this.temperature >= 4000) {
+            setAnimation(this.hotAnimation)
+
+        } else {
+            setAnimation(this.normalAnimation);
         }
-
     }
 }
+
